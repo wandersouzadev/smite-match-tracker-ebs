c47fe6008e3286962a38a58b23b58f952dcc5583
Wander Souza <wandersouza@live.com>
2023-02-06 22:22:04 -0300

    add god ranks method types

---
+++ src/smite/types/get-god-ranks.ts
@@ -0,0 +1,14 @@
+export class GetGodRanks {
+  Assists: number;
+  Deaths: number;
+  Kills: number;
+  Losses: number;
+  MinionKills: number;
+  Rank: number;
+  Wins: number;
+  Worshippers: number;
+  god: string;
+  god_id: string;
+  player_id: string;
+  ret_msg: any;
+}
